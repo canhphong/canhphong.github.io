0d9be5e542143e9ca3b1e1de19812588ff52cc53
canhphong <canhphong650@gmail.com>
2017-11-27 07:00:50 +0700

    update js for calendar

--- Javascript/Calender/js/calender.js
+++ Javascript/Calender/js/calender.js
@@ -1,79 +1,70 @@
-// Loop Option of year
-var start = 1900;
-var end = new Date().getFullYear();
-var options = "";
-for(var year = start ; year <=end; year++){
-  options += "<option>"+ year +"</option>";
-}
-document.getElementById("year").innerHTML = options;
+/*
+Calendar JS
+*/
+var dateNow = new Date();
+var currDay = dateNow.getDate();
+var currMon = dateNow.getMonth();
+var currYear = dateNow.getFullYear();
+var cellDay = document.getElementsByTagName('td');
+var listMon = document.getElementById("select-months");
+var listYear = document.getElementById("select-years");
+drawCalender(currYear, currMon);
+ showlistYear();
+chooseAnyDay();
 
-//Loop Option of month
+// Draw Calendar 
+function drawCalender(year, month) {
+	parseInt(year);
+	parseInt(month);
+	var firstDay = new Date(year, month, 1).getDay();
+	var lastDate = new Date(year, month + 1, 0).getDate();
+	var i, day;
+	day = 13 + firstDay;
 
-
-// Write function
-
-function isFourDigitYear(year) {
-	if(year.length != 4) {
-		document.calControl.year.select();
-		document.calControl.year.focus();
+	for (var i = 13; i < 55; i++) {
+		cellDay[i].innerHTML = "";
+		cellDay[i].style.backgroundColor = "#fff";
 	}
-	else {return true;}
-}
 
-function selectDate() {
-	var year = document.calControl.year.value;
-	if (isFourDigitYear(year)){
-		var day = 0;
-		var month = document.calControl.month.selectedIndex;
-		displayCalendar(month, year);
+	for (var i = 1; i <= lastDate; i++) {
+		cellDay[day].innerHTML = i;
+		day++;
 	}
-}
-// Moved to the previous Year
-function setPreviousYear() {
-	var year = document.calControl.year.value;
-	if(isFourDigitYear(year)) {
-		var day = 0;
-		var month = document.calControl.month.selectedIndex;
-		year--;
-		document.calControl.year.value = year;
-		displayCalendar(month, year);
-	}
-}
 
-// Moved to the previous Month
-function setPreviousMonth() {
-	var year = document.calControl.year.value;
-	if(isFourDigitYear(year)) {
-		var day = 0;
-		var month = document.calControl.month.selectedIndex;
-		if(month == 0) {
-			month = 11;
-			if(year > 1000) {
-				year--;
-				document.calControl.year.value = year;
-			}
-		}
-		else {month--;}
-		document.calControl.month.selectedIndex = month;
-		displayCalendar(month, year);
-	}
-}
-// Moved to the next Year
-function setNextMonth() {
-	var year = document.calControl.year.value;
-	if(isFourDigitYear(year)) {
-		var day = 0;
-		var month = document.calControl.month.selectedIndex;
-		if (month == 11) {
-			month = 0;
-			year++;
-			document.calControl.year.value = year;
+	if (year === dateNow.getFullYear()) {
+		if (month === dateNow.getMonth()) {
+			var showCurdate = 12 + firstDay + dateNow.getDate();
+			cellDay[showCurdate].style.backgroundColor = "red";
 		} else {
-			month++;
+			return true;
 		}
-		document.calControl.month.selectedIndex = month;
-		displayCalendar(month, year);
 	}
+	showTimeBox();
 }
 
+//show list Year in combobox
+function showlistYear() {
+	for (var i = 1954; i < 2100; i++) {
+		listYear.innerHTML += "<option value='" + i + "'>" + i + "</option>";
+	}
+	drawCalender(currYear, currMon);
+}
 
+function showTimeBox() {
+	listMon.value = currMon;
+	listYear.value = currYear;
+}
+
+function chooseMonth(month) {
+	currMon += month;
+	if (currMon < 0) {
+		currMon = 11;
+		currYear -= 1;
+	}
+	if (currMon > 11) {
+		currMon = 0;
+		currYear += 1;
+	}
+	checkTime();
+	drawCalender(currYear, currMon);
+}
