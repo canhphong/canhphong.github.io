6d3ace0bafa6989f9a2c64a1024c321183a76aad
canhphong <canhphong650@gmail.com>
2017-11-27 15:22:21 +0700

    edit template form ajax

--- Javascript/FormAjax/index.php
+++ Javascript/FormAjax/index.php
@@ -12,35 +12,29 @@
 <body>
 	<div class="container">
 		<div class="signup">
-				<form action="/sync.php" method="post">
-		<table class="form-ajax">
-		<tr>
-			<td><label>Username:</label></td>
-			<td><input type="text" name="username" placeholder="Enter Username"></td>
-		</tr>
-		<tr>
-			<td><label>Password:</label></td>
-			<td><input type="password" name="password" placeholder="Enter Password"></td>
-		</tr>
-		<tr>
-			<td><label>Email:</label></td>
-			<td><input type="email" name="email" placeholder="Enter Email"></td>
-		</tr>
-		<tr>
-			<td><label>Birthday:</label></td>
-			<td><?php include 'calendar.php'; ?></td>
-		</tr>
-		<tr>
-			<td></td>
-			<td>
-				<input type="submit" value="SUBMIT">
-				<input type="reset" value="REFREST">
-			</td>
-		</tr>
-	</table>
-</form>
+			<form action="#" method="post" name="signup" >
+				<p>
+					<label for="username">Username</label>
+					<input type="text" name="username" id="username">
+				</p>
+				<p>
+					<label for="password">Password</label>
+					<input type="text" name="password" id="password">
+				</p>
+				<p>
+					<label for="email">Email</label>
+					<input type="text" name="email" id="email">
+				</p>
+				<p>
+					<label for="birthday">Birthday</label>
+					<?php include_once 'calendar.php'; ?>
+				</p>
+				<p class="btn-submit">
+					<label for=""><input type="submit" value="SUBMIT"></label>
+					<input type="reset" value="REFREST">
+				</p>
+			</form>
 		</div>
-	</div>
       <script src="js/formajax.js"></script>
       <script src="js/calendar.js"></script>
 </body>
