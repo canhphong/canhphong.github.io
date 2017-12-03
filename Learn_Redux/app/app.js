feb11e651b34803c7bcaa14ebc00f38d79b0165e
canhphong <canhphong650@gmail.com>
2017-12-03 21:07:02 +0700

    add folders Learn_Redux KhoaPham

---
+++ Learn_Redux/app/app.js
@@ -0,0 +1,10 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var List = require('./components/List');
+
+ReactDOM.render(
+    <List/>,
+  document.getElementById('root')
+);
+
+require('./examples.js');
