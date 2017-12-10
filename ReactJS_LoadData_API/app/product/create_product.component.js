e3fe1e248128ba86c95bc5ac9f2fdf380f5c35f3
canhphong <canhphong650@gmail.com>
2017-12-10 13:11:43 +0700

    fix bug Create and Read Data

--- ReactJS_LoadData_API/app/product/create_product.component.js
+++ ReactJS_LoadData_API/app/product/create_product.component.js
@@ -1,102 +1,98 @@
 window.CreateProductComponent = React.createClass({
-    // initial component states will be here
-    // initialize values
-getInitialState: function() {
+  // initial component states will be here
+  // initialize values
+  getInitialState: function() {
     return {
-        categories: [],
-        selectedCategoryId: -1,
-        name: '',
-        description: '',
-        price: '',
-        successCreation: null
+      categories: [],
+      selectedCategoryId: -1,
+      name: '',
+      description: '',
+      price: '',
+      successCreation: null
     };
-},
+  },
 
-// on mount, get all categories and store them in this component's state
-componentDidMount: function() {
-    this.serverRequest = $.get("http://localhost/canhphong.github.io/ReactJS_API/api/category/read.php", function (categories) {
-        this.setState({
-            categories: categories.records
-        });
+  // on mount, get all categories and store them in this component's state
+  componentDidMount: function() {
+    this.serverRequest = $.get("http://localhost/canhphong.github.io/ReactJS_API/api/category/read.php", function(categories) {
+      this.setState({categories: categories.records});
     }.bind(this));
 
     $('.page-header h1').text('Create product');
-},
+  },
 
-// on unmount, stop getting categories in case the request is still loading
-componentWillUnmount: function() {
+  // on unmount, stop getting categories in case the request is still loading
+  componentWillUnmount: function() {
     this.serverRequest.abort();
-},
+  },
 
-// handle form field changes here
+  // handle form field changes here
 
-// handle category change
-onCategoryChange: function(e) {
+  // handle category change
+  onCategoryChange: function(e) {
     this.setState({selectedCategoryId: e.target.value});
-},
+  },
 
-// handle name change
-onNameChange: function(e) {
+  // handle name change
+  onNameChange: function(e) {
     this.setState({name: e.target.value});
-},
+  },
 
-// handle description change
-onDescriptionChange: function(e) {
+  // handle description change
+  onDescriptionChange: function(e) {
     this.setState({description: e.target.value});
-},
+  },
 
-// handle price change
-onPriceChange: function(e) {
+  // handle price change
+  onPriceChange: function(e) {
     this.setState({price: e.target.value});
-},
+  },
 
-// handle save button here
-// handle save button clicked
-onSave: function(e){
+  // handle save button here
+  // handle save button clicked
+  onSave: function(e) {
 
     // data in the form
-    var form_data={
-        name: this.state.name,
-        description: this.state.description,
-        price: this.state.price,
-        category_id: this.state.selectedCategoryId
+    var form_data = {
+      name: this.state.name,
+      description: this.state.description,
+      price: this.state.price,
+      category_id: this.state.selectedCategoryId
     };
 
     // submit form data to api
     $.ajax({
-        url: "http://localhost/canhphong.github.io/ReactJS_API/api/product/create.php",
-        type : "POST",
-        contentType : 'application/json',
-        data : JSON.stringify(form_data),
-        success : function(response) {
+      url: "http://localhost/canhphong.github.io/ReactJS_API/api/product/create.php",
+      type: "POST",
+      contentType: 'application/json',
+      data: JSON.stringify(form_data),
+      success: function(response) {
 
-            // api message
-            this.setState({successCreation: response['message']});
+        // api message
+        this.setState({successCreation: response['message']});
 
-            // empty form
-            this.setState({name: ""});
-            this.setState({description: ""});
-            this.setState({price: ""});
-            this.setState({selectedCategoryId: -1});
+        // empty form
+        this.setState({name: ""});
+        this.setState({description: ""});
+        this.setState({price: ""});
+        this.setState({selectedCategoryId: -1});
 
-        }.bind(this),
-        error: function(xhr, resp, text){
-            // show error to console
-            console.log(xhr, resp, text);
-        }
+      }.bind(this),
+      error: function(xhr, resp, text) {
+        // show error to console
+        console.log(xhr, resp, text);
+      }
     });
 
     e.preventDefault();
-},
+  },
 
-// render component here
-render: function() {
+  // render component here
+  render: function() {
 
     // make categories as option for the select tag.
-    var categoriesOptions = this.state.categories.map(function(category){
-        return (
-            <option key={category.id} value={category.id}>{category.name}</option>
-        );
+    var categoriesOptions = this.state.categories.map(function(category) {
+      return (<option key={category.id} value={category.id}>{category.name}</option>);
     });
 
     /*
@@ -105,31 +101,28 @@ render: function() {
     - button to go back to products list
     - form to create a product
     */
-    return (
-    <div>
+    return (<div>
       {
 
-        this.state.successCreation == "Product was created." ?
-          <div className='alert alert-success'>
-            Product was saved.
-          </div>
-        : null
+        this.state.successCreation == "Product was created."
+          ? <div className='alert alert-success'>
+              Product was saved.
+            </div>
+          : null
       }
 
       {
 
-        this.state.successCreation == "Unable to create product." ?
-          <div className='alert alert-danger'>
-            Unable to save product. Please try again.
-          </div>
-        : null
+        this.state.successCreation == "Unable to create product."
+          ? <div className='alert alert-danger'>
+              Unable to save product. Please try again.
+            </div>
+          : null
       }
 
-      <a href='#'
-        onClick={() => this.props.changeAppMode('read')}
-      className='btn btn-primary margin-bottom-1em'> Read Products
+      <a href='#' onClick={() => this.props.changeAppMode('read')} className='btn btn-primary margin-bottom-1em'>
+        Read Products
       </a>
-
 
       <form onSubmit={this.onSave}>
         <table className='table table-bordered table-hover'>
@@ -137,48 +130,28 @@ render: function() {
             <tr>
               <td>Name</td>
               <td>
-                <input
-                  type='text'
-                  className='form-control'
-                  value={this.state.name}
-                  required
-                  onChange={this.onNameChange} />
+                <input type='text' className='form-control' value={this.state.name} required="required" onChange={this.onNameChange}/>
               </td>
             </tr>
 
             <tr>
               <td>Description</td>
               <td>
-                <textarea
-                  type='text'
-                  className='form-control'
-                  required
-                  value={this.state.description}
-                  onChange={this.onDescriptionChange}>
-                </textarea>
+                <textarea type='text' className='form-control' required="required" value={this.state.description} onChange={this.onDescriptionChange}></textarea>
               </td>
             </tr>
 
             <tr>
               <td>Price ($)</td>
               <td>
-                <input
-                  type='number'
-                  step="0.01"
-                  className='form-control'
-                  value={this.state.price}
-                  required
-                  onChange={this.onPriceChange}/>
+                <input type='number' step="0.01" className='form-control' value={this.state.price} required="required" onChange={this.onPriceChange}/>
               </td>
             </tr>
 
             <tr>
               <td>Category</td>
               <td>
-                <select
-                  onChange={this.onCategoryChange}
-                  className='form-control'
-                  value={this.state.selectedCategoryId}>
+                <select onChange={this.onCategoryChange} className='form-control' value={this.state.selectedCategoryId}>
                   <option value="-1">Select category...</option>
                   {categoriesOptions}
                 </select>
@@ -188,15 +161,12 @@ render: function() {
             <tr>
               <td></td>
               <td>
-                <button
-                  className='btn btn-primary'
-                  onClick={this.onSave}>Save</button>
+                <button className='btn btn-primary' onClick={this.onSave}>Save</button>
               </td>
-                </tr>
-                </tbody>
-            </table>
-        </form>
-    </div>
-    );
-}
+            </tr>
+          </tbody>
+        </table>
+      </form>
+    </div>);
+  }
 });
