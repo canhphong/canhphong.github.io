5eb69963d3db52cdf18e7f4b7555650a34079870
canhphong <canhphong650@gmail.com>
2017-12-10 13:08:53 +0700

    fix bug Create and Read Data

--- ReactJS_LoadData_API/app/product/read_products.component.js
+++ ReactJS_LoadData_API/app/product/read_products.component.js
@@ -1,41 +1,82 @@
-// component that contains all the logic and other smaller components
-// that form the Read Products view
+// // component that contains all the logic and other smaller components
+// // that form the Read Products view
+// window.ReadProductsComponent = React.createClass({
+//     getInitialState: function() {
+//         return {
+//             products: []
+//         };
+//     },
+//
+//     // on mount, fetch all products and stored them as this component's state
+//     componentDidMount: function() {
+//
+//         this.serverRequest = $.get("http://localhost/canhphong.github.io/ReactJS_API/api/product/read.php", function (products) {
+//             this.setState({
+//                 products: products.records
+//             });
+//         }.bind(this));
+//     },
+//
+//     // on unmount, kill product fetching in case the request is still pending
+//     componentWillUnmount: function() {
+//         this.serverRequest.abort();
+//     },
+//
+//     // render component on the page
+//     render: function() {
+//         // list of products
+//         var filteredProducts = this.state.products;
+//         $('.page-header h1').text('Read Products');
+//
+//         return (
+//             <div className='overflow-hidden'>
+//                 <TopActionsComponent changeAppMode={this.props.changeAppMode} />
+//
+//                 <ProductsTable
+//                     products={filteredProducts}
+//                     changeAppMode={this.props.changeAppMode} />
+//             </div>
+//         );
+//     }
+// });
+
+
 window.ReadProductsComponent = React.createClass({
-  getInitialState: function() {
-    return {
-      products: []
-    };
-  },
+    getInitialState: function() {
+        return {
+            products: []
+        };
+    },
 
-// on mount, fetch all products and stored them as this components  state
-componentDidMount: function() {
-  this.serverRequest = $.get("http://localhost/canhphong.github.io/ReactJS_API/api/product/read.php", function(products) {
-    this.setState({
-      products : products.records
-    });
-  }.bind(this));
-},
+    // on mount, fetch all products and stored them as this component's state
+    componentDidMount: function() {
 
+        this.serverRequest = $.get("http://localhost/canhphong.github.io/ReactJS_API/api/product/read.php", function (products) {
+            this.setState({
+                products: products.records
+            });
+        }.bind(this));
+    },
 
-// on unmount, kill product fetching in case the request is still pending
-componentWillUnmount: function() {
-  this.serverRequest.abort();
-},
+    // on unmount, kill product fetching in case the request is still pending
+    componentWillUnmount: function() {
+        this.serverRequest.abort();
+    },
 
-// render component on the page
-render: function() {
-  // list of products
-  var filteredProducts = this.state.products;
-  $('.page-header h1').text('Read Products');
+    // render component on the page
+    render: function() {
+        // list of products
+        var filteredProducts = this.state.products;
+        $('.page-header h1').text('Read Products');
 
-  return(
-    <div className='overflow-hidden'>
-      <TopActionsComponent changeAppMode={this.props.changeAppMode} />
+        return (
+            <div className='overflow-hidden'>
+              <TopActionsComponent changeAppMode={this.props.changeAppMode} />
 
-      <ProductsTable
-        products = {filteredProducts}
-        changeAppMode = {this.props.changeAppMode} />
-    </div>
-  );
-}
+              <ProductsTable
+                    products={filteredProducts}
+                    changeAppMode={this.props.changeAppMode} />
+            </div>
+        );
+    }
 });
